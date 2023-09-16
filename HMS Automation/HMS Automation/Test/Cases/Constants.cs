caf445e401c68abedbcc62672330aab654e1361a
Sahanaj <sahanaj961@gmail.com>
2023-09-16 13:37:21 +0530

    constant page

---
+++ HMS Automation/HMS Automation/Test/Cases/Constants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS_Automation.Test.Cases
+{
+    internal class Constants
+    {
+        internal static string firstName = "Raj";
+        internal static string lastName = "Smith";
+        internal static long phoneNumber = 100;
+        internal static string dob = "12-09-2000";
+        internal static string address1 = "USA";
+        internal static string city = "test";
+        internal static string pin = "78909";
+        internal static string insuranceName = "LIC";
+        internal static string policyNumber = "123";
+        internal static string groupNumber = "34";
+
+    }
+}
--- HMS Automation/HMS Automation/Test/Cases/UnderPractice.cs
+++ HMS Automation/HMS Automation/Test/Cases/UnderPractice.cs
@@ -24,17 +24,17 @@ namespace HMS_Automation.Test.Cases
             IWebElement PatientCreate = driver.FindElement(By.XPath("//button[normalize-space()='Add Patient']"));
             PatientCreate.Click();
             IWebElement firstName = driver.FindElement(By.Id("mat-input-1"));
-            firstName.SendKeys("Raj");
+            firstName.SendKeys(Constants.firstName);
             IWebElement lastName = driver.FindElement(By.Id("mat-input-2"));
             Random random = new Random();
-            lastName.SendKeys("Smith"+ random.Next(100, 1000));
+            lastName.SendKeys(Constants.lastName + random.Next(100, 1000));
             IWebElement phoneNumber = driver.FindElement(By.Id("mat-input-3"));
             long randomNumber = random.Next(1000000, 1000000); // Generates a random number between 1,000,000,000 and 9,999,999,999
 
             phoneNumber.SendKeys("100"+randomNumber);
 
             IWebElement email=driver.FindElement(By.Id("mat-input-4"));
-            email.SendKeys("Raj" + random.Next(100, 1000) + "@gmail.com");
+            email.SendKeys(Constants.firstName + random.Next(100, 1000) + "@gmail.com");
 
            
             IWebElement genderDropdown = driver.FindElement(By.ClassName("mat-select-placeholder")); 
@@ -44,16 +44,16 @@ namespace HMS_Automation.Test.Cases
             genderSelect.Click();
 
             IWebElement dob = driver.FindElement(By.Id("dob"));
-            dob.SendKeys("12-09-2000");
+            dob.SendKeys(Constants.dob);
 
             IWebElement nextButton = driver.FindElement(By.XPath("(//button[@type='submit'][normalize-space()='Next'])[1]"));
             nextButton.Click();
 
             IWebElement Address1 = driver.FindElement(By.Id("mat-input-6"));
-            Address1.SendKeys("USA");
+            Address1.SendKeys(Constants.address1);
 
             IWebElement city = driver.FindElement(By.Id("mat-input-8"));
-            city.SendKeys("test");
+            city.SendKeys(Constants.city);
 
            
             IWebElement stateDropDown = driver.FindElement(By.ClassName("mat-select-placeholder"));
@@ -64,27 +64,27 @@ namespace HMS_Automation.Test.Cases
            
             state.Click();
             IWebElement pin = driver.FindElement(By.Id("mat-input-9"));
-            pin.SendKeys("78909");
+            pin.SendKeys(Constants.pin);
 
             IWebElement nextClick = driver.FindElement(By.XPath(" (//button[@type='submit'][normalize-space()='Next'])[2]"));
             nextClick.Click();
            IWebElement skipButton = driver.FindElement(By.XPath("(//button[@class='mat-stepper-next btn ms-3 patient-form-reset-btn position-absolute reset-btn'])[1]"));
            skipButton.Click();
             IWebElement insuranceName = driver.FindElement(By.Id("mat-input-13"));
-            insuranceName.SendKeys("LIC");
+            insuranceName.SendKeys(Constants.insuranceName);
             IWebElement policyNumber = driver.FindElement(By.Id("mat-input-14"));
-            policyNumber.SendKeys("1" + random.Next(100, 1000));
+            policyNumber.SendKeys(Constants.policyNumber + random.Next(100, 1000));
             IWebElement policyfirstName = driver.FindElement(By.Id("mat-input-15"));
-            policyfirstName.SendKeys("Raj");
+            policyfirstName.SendKeys(Constants.firstName);
             IWebElement policylastName = driver.FindElement(By.Id("mat-input-16"));
-            policylastName.SendKeys("Smith" + random.Next(100, 1000));
+            policylastName.SendKeys(Constants.lastName + random.Next(100, 1000));
             IWebElement groupNumber = driver.FindElement(By.Id("mat-input-17"));
-            groupNumber.SendKeys("2" + random.Next(100, 1000));
+            groupNumber.SendKeys(Constants.groupNumber + random.Next(100, 1000));
             IWebElement skipClick = driver.FindElement(By.XPath(" (//button[@class='btn ms-3 patient-form-reset-btn position-absolute reset-btn'])[1]"));
             skipClick.Click();
             IWebElement skip2Click= driver.FindElement(By.XPath("(//button[@class='mat-stepper-next btn ms-3 reset-btn'])[1]"));
             skip2Click.Click();
-            IWebElement languagedropdown = driver.FindElement(By.ClassName("mat-select-placeholder"));
+            /*IWebElement languagedropdown = driver.FindElement(By.ClassName("mat-select-placeholder"));
             languagedropdown.Click();
             IWebElement language = driver.FindElement(By.XPath("(//mat-option[@id='mat-option-5'])[1]"));
             language.Click();
@@ -98,7 +98,7 @@ namespace HMS_Automation.Test.Cases
             monitoringDropdown.Click();
             IWebElement monitoring = driver.FindElement(By.XPath("(//mat-option[@id='mat-option-73'])[1]"));
             monitoring.Click();
-            IWebElement patientEMRId = driver.FindElement(By.XPath("mat-input-23"));
+           */ IWebElement patientEMRId = driver.FindElement(By.XPath("mat-input-23"));
             patientEMRId.SendKeys("1"+ random.Next(100, 1000));
 
             IWebElement submit = driver.FindElement(By.XPath("(//button[normalize-space()='Submit'])[1]"));
