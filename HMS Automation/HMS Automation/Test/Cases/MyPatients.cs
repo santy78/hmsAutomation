7151d99e1cca5314f4c167ec454e31bd149144d0
Sahanaj <sahanaj961@gmail.com>
2023-09-19 12:19:29 +0530

    my patient training report

--- HMS Automation/HMS Automation/Test/Cases/MyPatients.cs
+++ HMS Automation/HMS Automation/Test/Cases/MyPatients.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using HMS_Automation.Model;
+using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,37 @@ namespace HMS_Automation.Test.Cases
 {
     internal class MyPatients
     {
+        public static int SessionId;
         public void Patients(IWebDriver driver)
         {
-            IWebElement myPatients = driver.FindElement(By.XPath("(//a[normalize-space()='MY PATIENTS'])[1]"));
-            myPatients.Click();
+
+            HMSAutomationResult automationresult = new HMSAutomationResult();
+            HMSAutomationDBContext automationDBContext = new HMSAutomationDBContext();
+            automationresult.BatchId = SessionId;
+            automationresult.ScreenName = "PATIENTS";
+            automationresult.ResponseType = "";
+            automationresult.Request = "";
+            automationresult.Response = "";
+            automationresult.Errors = "";
+            automationresult.DateTime = DateTime.Now.ToString();
+            try
+            {
+                IWebElement myPatients = driver.FindElement(By.XPath("(//a[normalize-space()='MY PATIENTS'])[1]"));
+                myPatients.Click();
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+                automationresult.ResponseType = "PASS";
+            }
+            catch (NoSuchElementException ex)
+            {
+                // Handle the case where the element is not found
+                Console.WriteLine("Element not found: " + ex.Message);
+                automationresult.ResponseType = "FAIL";
+                automationresult.Errors = e.Message;
+            }
+            automationDBContext.SaveAutomationResult(automationresult);
+
+
+
         }
     }
 }
--- HMS Automation/HMS Automation/Test/Cases/TrainingReport.cs
+++ HMS Automation/HMS Automation/Test/Cases/TrainingReport.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using HMS_Automation.Model;
+using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,15 +10,40 @@ namespace HMS_Automation.Test.Cases
 {
     internal class TrainingReport
     {
+        public static int SessionId;
         public void ReportOfTraining(IWebDriver driver)
         {
-            IWebElement report = driver.FindElement(By.XPath("(//a[normalize-space()='REPORTS'])[1]"));
-            report.Click();
+            SessionId = HMSAutomationDBContext.GetBatchID();
+            HMSAutomationResult automationresult = new HMSAutomationResult();
+            HMSAutomationDBContext automationDBContext = new HMSAutomationDBContext();
+            automationresult.BatchId = SessionId;
+            automationresult.ScreenName = "Training Report";
+            automationresult.ResponseType = "";
+            automationresult.Request = "";
+            automationresult.Response = "";
+            automationresult.Errors = "";
+            automationresult.DateTime = DateTime.Now.ToString();
 
-            IWebElement trainingReport = driver.FindElement(By.XPath("(//li[@role='tab'])[1]"));
-            trainingReport.Click();
+            try
+            {
+                // Find and click on the "REPORTS" element
+                IWebElement report = driver.FindElement(By.XPath("(//a[normalize-space()='REPORTS'])[1]"));
+                report.Click();
+                Thread.Sleep(TimeSpan.FromSeconds(3));
+                // Find and click on the "trainingReport" element
+                IWebElement trainingReport = driver.FindElement(By.XPath("(//li[@role='tab'])[1]"));
+                trainingReport.Click();
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                automationresult.ResponseType = "PASS";
+            }
+            catch (NoSuchElementException ex)
+            {
+                // Handle the case where the element is not found
+                Console.WriteLine("Element not found: " + ex.Message);
+                automationresult.ResponseType = "FAIL";
+                automationresult.Errors = ex.Message;
+            }
 
-            
         }
     }
 }
